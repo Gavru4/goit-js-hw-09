241aea751bc59d96f5ebdc187353c6a013ddb3ac
Roman Havryliuk <gavru4r@gmail.com>
2022-01-09 18:56:12 +0200

    finished task 1 hw-9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStart = document.querySelector("button[data-start]")
+const btnStop = document.querySelector("button[data-stop]")
 
+let timerId = null;
+
+const onBtnStartClick = btnStart.addEventListener("click", startChangeColor)
+
+function startChangeColor() {
+
+    timerId = setTimeout(startChangeColor, 1000);
+    
+    document.body.style.backgroundColor = getRandomHexColor();
+
+     const btnDisable =  btnStart.setAttribute('disabled','disabled')
+    
+}
+
+const onBtnStopClick = btnStop.addEventListener("click", stopChangeColor)
+ 
+function stopChangeColor() {
+    clearTimeout(timerId);
+    const removeAttribute = btnStart.removeAttribute("disabled");
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
